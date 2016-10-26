61c2cd5a1ef1c68b17cbfc16951512998c75a8ec
Jianqiu Zhang <zhangjianqiu13@gmail.com>
2016-10-26 20:47:49 +0800

    Create API Needed Structure

--- config/config.go
+++ config/config.go
@@ -1,5 +1,7 @@
 package config
 
+var GlobalConfig SystemConfig
+
 type SystemConfig struct {
 	HostName         string `toml:"host_name"`
 	EndpointName     string `toml:"endpoint_name"`
@@ -8,5 +10,41 @@ type SystemConfig struct {
 	EndpointPassword string `toml:"endpoint_password"`
 	JudgeRoot        string `toml:"judge_root"`
 	DockerImage      string `toml:"docker_image"`
+	DockerServer     string `toml:"docker_server"`
 	CacheRoot        string `toml:"cache_root"`
+}
+
+type JudgeInfo struct {
+	SubmitID      int64  `json:"submitid"`
+	ContestID     int64  `json:"cid"`
+	TeamID        int64  `json:"teamid"`
+	ProblemID     int64  `json:"probid"`
+	Language      string `json:"langid"`
+	TimeLimit     int64  `json:"maxruntime"`
+	MemLimit      int64  `json:"memlimit"`
+	OutputLimit   int64  `json:"output_limit"`
+	BuildZip      string `json:"compile_script"`
+	BuildZipMD5   string `json:"compile_script_md5sum"`
+	RunZip        string `json:"run"`
+	RunScriptMD5  string `json:"run_md5sum"`
+	CompareZip    string `json:"compare"`
+	CompareZipMD5 string `json:"compare_md5"`
+	CompareArgs   string `json:"compare_args"`
+}
+
+type TestcaseInfo struct {
+	TestcaseID   int64  `json:"testcaseid"`
+	Rank         int64  `json:"rank"`
+	ProblemID    int64  `json:"probid"`
+	MD5SumInput  string `json:"md5sum_input"`
+	MD5SumOutput string `json:"md5sum_input"`
+}
+
+type SubmissionInfo struct {
+	info []SubmissionFileInfo `json:""`
+}
+
+type SubmissionFileInfo struct {
+	FileName string `json:"filename"`
+	Content  string `json:"contetn"`
 }
